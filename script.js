2c3065bdf98532bc0e157a3d5da333f3809a6ecc
Oscar <odgarrett@ucdavis.edu>
2025-04-27 10:10:11 -0700

    Refactor, clean, and add parameters at the top

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
-const canvas = document.getElementById("canvas1"); // selects the canvas html tag
-const ctx = canvas.getContext("2d"); // uses html tag to select the canvas 2d rendering object, which has lots of different properties and methods used for drawing in the canvas
+// Parameters
+const N_PARTICLES = 5;
+const MAX_SIZE = 10;
+const MAX_SPEED = 2;
+const DECAY_RATE = 0.03;
+const TRAILS = true;
+const TRAIL_FADE = 0.2;
 
-// normalize canvas dimensions so that dimensions act more intuitively
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-// renormalize canvas dimensions and redraw rectangle when you resize the window
-window.addEventListener("resize", function () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
-
-// make a structure to hold newly generated particles
 const particlesArray = [];
-
-// create a variable to store the hue
 let hue = 0;
 
-// to add interactivity
-// define a mouse object for global accessibility
 const mouse = {
   x: undefined,
   y: undefined,
 };
 
-// add an event listenter to update the global mouse
-// object with the x, y coordinates of a click
-canvas.addEventListener("click", function (event) {
-  mouse.x = event.x;
-  mouse.y = event.y;
-  generateParticles(5);
+canvas.height = window.innerHeight;
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 });
 
-// more convenient to bundle drawing circles into a
-// function
-function drawCircle() {
-  ctx.fillStyle = "white";
-  ctx.beginPath();
-  // draw a circle centered at where you clicked, which
-  // was updated above
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-  ctx.fill();
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * MAX_SIZE + 1;
+    this.speedX = Math.random() * MAX_SPEED - 1;
+    this.speedY = Math.random() * MAX_SPEED - 1;
+    this.color = `hsl(${hue}, 100%, 50%)`;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= DECAY_RATE;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
 
 function generateParticles(n) {
   for (let i = 0; i < n; i++) particlesArray.push(new Particle());
 }
+
+canvas.addEventListener("click", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  generateParticles(N_PARTICLES);
+});
 
 function handleMouseMove() {
   mouse.x = event.x;
@@ -68,29 +77,6 @@ function throttle(callback, limit) {
 
 canvas.addEventListener("mousemove", throttle(handleMouseMove, 1000 / 20));
 
-class Particle {
-  constructor() {
-    this.x = mouse.x;
-    this.y = mouse.y;
-    this.size = Math.random() * 10 + 1;
-    this.speedX = Math.random() * 2 - 1;
-    this.speedY = Math.random() * 2 - 1;
-    this.color = `hsl(${hue}, 100%, 50%)`;
-  }
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.03;
-  }
-
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
-  }
-}
-
 function handleParticles() {
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
@@ -101,7 +87,7 @@ function handleParticles() {
       const distance = Math.sqrt(dx ** 2 + dy ** 2);
       if (distance < 100) {
         ctx.strokeStyle = particlesArray[i].color;
-        ctx.lineWidth = particlesArray[i].size / 10;
+        ctx.lineWidth = particlesArray[i].size / 8;
         ctx.beginPath();
         ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
         ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
@@ -117,20 +103,17 @@ function handleParticles() {
   }
 }
 
-// making an animation
 function animate() {
-  //// first, clear the whole canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // fade out the canvas to give particles trails
-  //ctx.fillStyle = 'rgba(0,0,0,0.01)'
-  //ctx.fillRect(0,0, canvas.width, canvas.height);
-
+  if (!TRAILS) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  if (TRAILS) {
+    // fade out the canvas to give particles trails
+    ctx.fillStyle = `rgba(0,0,0,${TRAIL_FADE})`;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
   handleParticles();
-
   hue += 0.5;
-
-  // call the animate function again, creating a loop
   requestAnimationFrame(animate);
 }
 
